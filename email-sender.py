e1ff762607525477d69387d50675ccbcd693aaee
Andresgbe <98847139+Andresgbe@users.noreply.github.com>
2023-01-21 16:10:48 -0400

    Add files via upload

---
+++ email-sender.py
@@ -0,0 +1,17 @@
+import smtplib
+
+email_transmitter = "your email"
+password = "your password"
+email_receiver="receiver email"
+message = "the message"
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.starttls
+server.login(email_transmitter,password)
+
+for i in range(0,0):                          
+    server.sendmail(email_transmitter, email_receiver, message)
+    print("Sent messages:", i)
+server.quit()
+
+# By @Andresgbe
